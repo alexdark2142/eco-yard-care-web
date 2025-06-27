fae709ad490bbd2eaabd08b8e9d910e43dd8c7a5
Alex <alexdark2149@gmail.com>
2025-06-26 23:33:03 -0600

    chore: update server port configuration to use environment variable in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
